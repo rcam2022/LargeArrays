0817e66a52889d80eec533d4740b4b143bd868c5
Jack Pace <jpace2@harding.edu>
2022-05-06 14:55:38 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        int[][][][][][][][][][][][][][] arr = new int[4][4][4][4][4][4][4][4][4][4][4][4][4][4];
+        Arrays.assign(arr);
+        Arrays.print(arr);
+    }
+}
